e69f1a8c04ccc329fbfaa28cc1a2e92751092d0b
Maia99-code <maiapaniagua199@gmail.com>
2024-06-03 23:20:07 -0400

    new commit

--- src/App.js
+++ src/App.js
@@ -24,24 +24,13 @@ function App() {
   };
 
   return (
-    <div>
+    <>
       <Forms
         handleSubmit={handleSubmit}
         handleChange={handleChange}
         data={data}
       />
-
-      <div className="data--text">
-        <h2>Your Form Data:</h2>
-        <div>
-          <p> <span>First Name: </span> {data.firstName}</p>
-          <p> <span>Last Name:</span>  {data.lastName}</p>
-          <p> <span>Email:</span> {data.email}</p>
-          <p> <span>Password:</span> {data.password} </p>
-          <p> <span>Confirm Password:</span> {data.confirmPassword}</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 export default App;
--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,36 +1,122 @@
-export default function Forms({handleChange, handleSubmit, data}) {
-    
-    return(
-        <div className="form-container">
-      <h2>Formulario con Hooks</h2>
+import React, { useState } from 'react';
+
+export default function Forms({ handleChange, handleSubmit, data }) {
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const validateField = (name, value) => {
+    let newErrors = { ...errors };
+
+    switch (name) {
+      case 'firstName':
+        if (value.length < 2) {
+          newErrors.firstName = 'First Name must be at least 2 characters long';
+        } else {
+          newErrors.firstName = '';
+        }
+        break;
+      case 'lastName':
+        if (value.length < 2) {
+          newErrors.lastName = 'Last Name must be at least 2 characters long';
+        } else {
+          newErrors.lastName = '';
+        }
+        break;
+      case 'email':
+        if (value.length < 5) {
+          newErrors.email = 'Email must be at least 5 characters long';
+        } else {
+          newErrors.email = '';
+        }
+        break;
+      case 'password':
+        if (value.length < 8) {
+          newErrors.password = 'Password must be at least 8 characters long';
+        } else {
+          newErrors.password = '';
+        }
+        if (value !== data.confirmPassword) {
+          newErrors.confirmPassword = 'Passwords must match';
+        } else {
+          newErrors.confirmPassword = '';
+        }
+        break;
+      case 'confirmPassword':
+        if (value !== data.password) {
+          newErrors.confirmPassword = 'Passwords must match';
+        } else {
+          newErrors.confirmPassword = '';
+        }
+        break;
+      default:
+        break;
+    }
+
+    setErrors(newErrors);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(e);
+    validateField(name, value);
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Mas Formularios</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>First Name
-          <input type="text" name="firstName" value={data.firsName} onChange={handleChange} />
+          <label>
+            First Name
+            <input type="text" name="firstName" value={data.firstName} onChange={handleInputChange} />
           </label>
+          <span>
+            {errors.firstName && <p className="error">{errors.firstName}</p>}
+          </span>
         </div>
         <div>
-          <label>Last Name
-          <input type="text" name="lastName" value={data.lastName} onChange={handleChange} />
+          <label>
+            Last Name
+            <input type="text" name="lastName" value={data.lastName} onChange={handleInputChange} />
           </label>
+          <span>
+            {errors.lastName && <p className="error">{errors.lastName}</p>}
+          </span>
         </div>
         <div>
-          <label>Email
-          <input type="email" name="email" value={data.email} onChange={handleChange} />
+          <label>
+            Email
+            <input type="email" name="email" value={data.email} onChange={handleInputChange} />
           </label>
+          <span>
+            {errors.email && <p className="error">{errors.email}</p>}
+          </span>
         </div>
         <div>
-          <label>Password
-          <input type="password" name="password" value={data.password} onChange={handleChange} />
+          <label>
+            Password
+            <input type="password" name="password" value={data.password} onChange={handleInputChange} />
           </label>
+          <span>
+            {errors.password && <p className="error">{errors.password}</p>}
+          </span>
         </div>
         <div>
-          <label>Confirm your password
-          <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={handleChange} />
+          <label>
+            Confirm your password
+            <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={handleInputChange} />
           </label>
+          <span>
+            {errors.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
+          </span>
         </div>
-      </form>
         <button type="submit">Enviar</button>
-      </div>
-    )
+      </form>
+    </div>
+  );
 }
